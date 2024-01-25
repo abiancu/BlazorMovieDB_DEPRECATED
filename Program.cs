eb7e26050102a974715e5110988a470660381c7d
Alejandro Biancucci <alejandrobiancucci@gmail.com>
2024-01-25 08:41:19 -0600

    init commit: add secrets for local development

--- Program.cs
+++ Program.cs
@@ -2,9 +2,12 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorMovieDB;
 
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+var apiKey = builder.Configuration["AppSettings:API_KEY"];
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
