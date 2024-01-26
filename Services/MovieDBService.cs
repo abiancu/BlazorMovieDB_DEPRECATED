927c2871a746d02e97ad55d25bb78f7c69fa769d
Alejandro Biancucci <alejandrobiancucci@gmail.com>
2024-01-25 20:57:56 -0600

    add service to page; wip

--- Services/MovieDBService.cs
+++ Services/MovieDBService.cs
@@ -1,6 +1,7 @@
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-
+using BlazorMovieDB.Utilities;
 
 namespace BlazorMovieDB.Services 
 {    
@@ -8,13 +9,37 @@ namespace BlazorMovieDB.Services
     {
         private readonly HttpClient _httpClient;
         private readonly IConfiguration _config;
+        private readonly string? _apiKey;
 
         public MovieDBService(HttpClient httpClient, IConfiguration config)
         {
             _httpClient = httpClient;
             _config = config;
+            _apiKey = config.GetValue<string>("AppSettings:API_KEY");
         }
 
+        public async Task<HttpResponseMessage> GetMovies()
+        {
+            try
+            {
+                HttpResponseMessage? response = await _httpClient.GetAsync($"{Constants.baseUri}{Constants.discoverMovie}?apiKey={_apiKey}");
+                
+                if (response is not null) 
+                {
+                    return response;
+                    
+                }else
+                {
+                    return new HttpResponseMessage(System.Net.HttpStatusCode.NotFound);
+                }
+                
+            }
+            catch (HttpRequestException ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Error: {ex}");
+                throw;
+            }
 
+        }
     }
 }
--- Utilities/Constants.cs
+++ Utilities/Constants.cs
@@ -1,7 +1,8 @@
 namespace BlazorMovieDB.Utilities
 {
-    public static class APIConstant
+    public static class Constants
     {
-        
+        public const string baseUri = "https://api.themoviedb.org/3/";
+        public const string discoverMovie = "discover/movie";
     }
 }
